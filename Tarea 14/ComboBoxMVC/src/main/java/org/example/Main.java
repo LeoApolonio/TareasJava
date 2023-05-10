492a5c544ff7cfba02cee15d5cfa2535afc118fe
LeoApolonio <124628254+LeoApolonio@users.noreply.github.com>
2023-05-10 17:58:16 -0600

    Add files via upload

---
+++ Tarea 14/ComboBoxMVC/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+/*
+    Apolonio Villagómez Leonardo
+    2208
+    Matutino
+*/
+public class Main {
+    public static void main(String[] args) {
+        Ventana ventana = new Ventana("ComboBox MVC");
+    }
+
+}
